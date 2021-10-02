49c856cf29683f00025946ec6308241623ab137c
Nabiya Alam <nabiya@csslab1h.uwb.edu>
2021-10-02 00:52:55 -0700

    Fixed code and code coverage

--- main.cpp
+++ main.cpp
@@ -18,9 +18,15 @@ void test02() {
   assert(fact(5) == 120);
 }
 
+void test03() { assert(fact(31) == -1); }
+
+void test04() { unusedFunction(); }
+
 int main() {
   test01();
   test02();
+  test03();
+  test04();
   memoryLeakFunction();
 
   cout << "Done." << endl;
